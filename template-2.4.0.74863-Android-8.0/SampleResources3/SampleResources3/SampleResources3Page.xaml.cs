2a4efc3cee11540e440a83f1d8fff27c2eea937f
Igor Kovalchuk <igorko@chestnutcorp.com>
2017-11-15 01:29:48 +0200

    template-2.4.0.74863-Android-8.0 - initial commit

---
+++ template-2.4.0.74863-Android-8.0/SampleResources3/SampleResources3/SampleResources3Page.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Xamarin.Forms;
+
+namespace SampleResources3
+{
+    public partial class SampleResources3Page : ContentPage
+    {
+        public SampleResources3Page()
+        {
+            InitializeComponent();
+        }
+    }
+}
