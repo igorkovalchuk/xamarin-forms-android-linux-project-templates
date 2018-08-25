1562d7a180267bfa39f83382cb8965f69d6aed89
Igor Kovalchuk <igorko@chestnutcorp.com>
2018-08-25 14:32:59 +0300

    template 2.5.0.121934 Android 7.1 netstandard1.1

---
+++ template-2.5.0.121934-7.1-ns1.1/SampleDotNet2/DotNet2/SampleDotNet2Page.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Xamarin.Forms;
+
+namespace SampleDotNet2
+{
+    public partial class SampleDotNet2Page : ContentPage
+    {
+        public SampleDotNet2Page()
+        {
+            InitializeComponent();
+        }
+    }
+}
