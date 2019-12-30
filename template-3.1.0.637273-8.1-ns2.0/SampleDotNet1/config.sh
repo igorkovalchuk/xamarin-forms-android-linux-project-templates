3c8a30c1cb84c7d4abaa94abf9ddec2fc069b934
Igor Kovalchuk <igorko76@gmail.com>
2019-12-30 23:24:39 +0200

    update for xamarin-android Linux build #2194 (Apr 17, 2019)

--- template-3.1.0.637273-8.1-ns2.0/SampleDotNet1/config.sh
+++ template-3.1.0.637273-8.1-ns2.0/SampleDotNet1/config.sh
@@ -7,8 +7,7 @@ APP_ACTIVITY=md51cbc34b4e0c459aca239424837674266.MainActivity
 APP_NAME=SampleDotNet1
 
 
-APP_DIR=/my_directory/template-3.1.0.637273-8.1-ns2.0/SampleDotNet1
-
+APP_DIR=/home/myhomedir/projects/template-3.1.0.637273-8.1-ns2.0/SampleDotNet1
 
 APP_DROID_DIR=$APP_DIR/Droid
 
@@ -24,6 +23,6 @@ PCL_PREFIX=$PCL_DIR/bin/Debug/netstandard2.0/$PCL_NAME
 
 ANDROID_OVERRIDE_DIR=/data/data/$APP_PACKAGE/files/.__override__
 
-ANDROID_SDK=/home/workerlamp/java/sdk
+ANDROID_SDK=/home/myhomedir/Android/Sdk
 
-AVD_NAME=Pixel_API_22
+AVD_NAME=Pixel_3a_XL_API_28
